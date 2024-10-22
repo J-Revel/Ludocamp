5e6675903bd1f808584843294b1d20ba580e10b6
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2024-10-22 18:32:29 +0200

    Docs & dialogues inherit UnlockableConfig

--- Assets/Scripts/Configs/DocumentConfig.cs
+++ Assets/Scripts/Configs/DocumentConfig.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DocumentConfig", menuName = "Scriptable Objects/DocumentConfig")]
-public class DocumentConfig : ScriptableObject
+public class DocumentConfig : UnlockableConfig
 {
     public string id;
     public RectTransform document_prefab;

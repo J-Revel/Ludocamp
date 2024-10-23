5d818f7e569a1ee442498d77c7e5e14c925abf01
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2024-10-23 17:36:12 +0200

    Doc folder & doc viewer base

---
+++ Assets/Scripts/DocumentMiniature.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DocumentMiniature : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private bool moveToFrontOnHover;
+    [SerializeField] private DocumentConfig documentConfig;
+    [SerializeField] private TMPro.TextMeshProUGUI shortTitleText;
+    [SerializeField] private TMPro.TextMeshProUGUI fullTitleText;
+    [SerializeField] private GameObject fullTitleTooltip;
+    //[SerializeField] private GameObject placeholderPrefab;
+    [SerializeField] private Transform tooltipContainer;
+    private int childIndex;
+    
+    void Awake()
+    {
+        shortTitleText.text = documentConfig.shortTitle;
+        fullTitleText.text = documentConfig.fullTitle;
+        fullTitleTooltip.gameObject.SetActive(false);
+        childIndex = transform.GetSiblingIndex();
+        float yOffset = fullTitleTooltip.transform.position.y - transform.position.y;
+        fullTitleTooltip.transform.rotation = Quaternion.identity;
+        fullTitleTooltip.transform.SetParent(tooltipContainer);
+        fullTitleTooltip.transform.position = this.transform.position + yOffset * Vector3.up;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("enter");
+        fullTitleTooltip.gameObject.SetActive(true);
+        if(moveToFrontOnHover) transform.SetAsLastSibling();
+        fullTitleTooltip.transform.SetAsFirstSibling();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        fullTitleTooltip.gameObject.SetActive(false);
+        transform.SetSiblingIndex(childIndex);
+        Debug.Log("child index = " + transform.GetSiblingIndex());
+    }
+}
